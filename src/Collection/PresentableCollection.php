ed6736d692257386c3da4a4770856969b5f733ad
Bogdan Cismariu <bogdan.cismariu@gmail.com>
2018-12-31 18:41:23 +0200

    fixed presenters

--- src/Collection/PresentableCollection.php
+++ src/Collection/PresentableCollection.php
@@ -2,19 +2,16 @@
 
 namespace Larafun\Suite\Collection;
 
-use Illuminate\Database\Eloquent\Collection as EloquentCollection;
-use Illuminate\Database\Query\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Larafun\Suite\Contracts\Queryable;
 use Larafun\Suite\Contracts\Presentable;
 use Larafun\Suite\Contracts\Transformable;
 use Larafun\Suite\Presenters\Presenter;
-use Larafun\Suite\Presenter\PaginationPresenter;
-use Larafun\Suite\Transformer\Transformer;
 use Larafun\Suite\Traits\QueryableTrait;
 use Larafun\Suite\Traits\PresentableTrait;
 use Larafun\Suite\Traits\TransformableTrait;
 
-class PresentableCollection extends EloquentCollection implements Queryable, Presentable, Transformable
+class PresentableCollection extends Collection implements Queryable, Presentable, Transformable
 {
     use QueryableTrait, PresentableTrait, TransformableTrait;
 
@@ -22,31 +19,13 @@ class PresentableCollection extends EloquentCollection implements Queryable, Pre
     {
         parent::__construct($items);
 
-        $this->presentWith(new Presenter($this));
+        $this->setPresenter(new Presenter($this));
     }
 
     public function toJson($options = 0)
     {
-        return $this->presenter()
+        return $this->getPresenter()
             ->toJson($options)
         ;
     }
-
-    public function present()
-    {
-        dd('present');
-        return $this->presenter()->present();
-    }
-
-    public function presentWith($presenter)
-    {
-        $this->presenter = $presenter;
-        return $this;
-    }
-
-    public function presenter()
-    {
-        return $this->presenter;
-    }
-
 }
--- src/Presenters/PresenterFactory.php
+++ src/Presenters/PresenterFactory.php
@@ -6,13 +6,13 @@ use Larafun\Suite\Contracts\Presenter;
 
 class PresenterFactory
 {
-    public static function make($instance = null, $data = null): Presenter
+    public static function make($presenter = null, $data = null): Presenter
     {
-        if (empty($instance)) {
-            $instance = PlainPresenter::class;
+        if (empty($presenter)) {
+            $presenter = PlainPresenter::class;
         }
-        if (is_string($instance)) {
-            $presenter = app($instance, compact('data'));
+        if (is_string($presenter)) {
+            $presenter = app($presenter, compact('data'));
         }
         return $presenter;
     }
